842e0a3456dc7867f4fbca28ef721afafca9f09f
Kalyan Kankala <kankala.kalyan@gmail.com>
2017-04-20 23:17:20 -0400

    added npm(package manager), express(web server), npmScripts(automation), babel(transpiling), webpack(bundling)

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,9 @@
+//ES 5/Old JavaScript way
+// var chalk = require('chalk');
+// console.log(chalk.green('Starting app in Dev mode...'))
+
+
+//ES6 way
+import chalk from 'chalk';
+
+console.log(chalk.green('Starting app in Dev mode...'))
